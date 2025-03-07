1f32e1c0e5e041dc581347293c3651b7ae6fb546
Kiran Koravi <kiran.koravi@buzzybrains.com>
2025-03-07 12:08:40 +0530

    first commit

---
+++ Home.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2017-present, Wonday (@wonday.org)
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { StyleSheet, Dimensions, View, Text } from 'react-native';
+import Pdf from 'react-native-pdf';
+
+export default class PDFExample extends React.Component {
+    render() {
+        const source = { uri: 'https://pdfobject.com/pdf/sample.pdf', cache: true };
+
+        return (
+            <View style={styles.container}>
+              <Text>333</Text>
+                <Pdf
+                 trustAllCerts={false}
+                    source={source}
+                    onLoadComplete={(numberOfPages,filePath) => {
+                        console.log(`Number of pages: ${numberOfPages}`);
+                    }}
+                    onPageChanged={(page,numberOfPages) => {
+                        console.log(`Current page: ${page}`);
+                    }}
+                    onError={(error) => {
+                        console.log(error);
+                    }}
+                    onPressLink={(uri) => {
+                        console.log(`Link pressed: ${uri}`);
+                    }}
+                    style={styles.pdf}/>
+                    
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 25,
+    },
+    pdf: {
+        flex:1,
+        width:Dimensions.get('window').width,
+        height:Dimensions.get('window').height,
+    }
+});
