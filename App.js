01342a0ea536319023f3dd761fbcde422cd99c18
Kiran Koravi <kiran.koravi@buzzybrains.com>
2025-04-07 18:15:15 +0530

    sentry added

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import PagerView from 'react-native-pager-view';
+import {StyleSheet, View, Text} from 'react-native';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://001268fbffd5f8421c89f95dc86bc372@o4508913177264128.ingest.us.sentry.io/4508913179230208',
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+});
 
 export default function App() {
-  return (
-    <PagerView style={styles.pagerView} initialPage={0}>
-      <View key="1">
-        <Text>First page</Text>
-      </View>
-      <View key="2">
-        <Text>Second page</Text>
-      </View>
-    </PagerView>
-  );
-};
+  function generateError() {
+    throw new Error('My first Sentry error!');
+  }
 
-const styles = StyleSheet.create({
-  pagerView: {
-    marginTop: 100,
-    backgroundColor: 'red',
-    flex: 1,
-  },
-});
+  return (
+    <View style={{ marginTop: 100 }}>
+      <Text onPress={generateError}>Hello World</Text>
+    </View>
+  );
+}
