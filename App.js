a6d5aa549117f620edd3f60b96aad8aedbc55b87
Kiran Koravi <kiran.koravi@buzzybrains.com>
2025-04-07 19:48:39 +0530

    lottie pkg added

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import * as Sentry from '@sentry/react-native';
+import React from "react";
+import LottieView from "lottie-react-native";
 
-Sentry.init({
-  dsn: 'https://001268fbffd5f8421c89f95dc86bc372@o4508913177264128.ingest.us.sentry.io/4508913179230208',
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
-});
-
-export default function App() {
-  function generateError() {
-    throw new Error('My first Sentry error!');
-  }
-
+export default function Animation() {
   return (
-    <View style={{ marginTop: 100 }}>
-      <Text onPress={generateError}>Hello World</Text>
-    </View>
+    <LottieView
+      source={require("./loading.json")}
+      style={{width: "100%", height: "100%"}}
+      autoPlay
+      loop={true}
+    />
   );
 }
