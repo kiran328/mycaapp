3fed11c2ac0b25f53a842c568dcb7cddfa78369a
Kiran Koravi <kiran.koravi@buzzybrains.com>
2025-04-07 17:32:33 +0530

    fixes

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import PagerView from 'react-native-pager-view';
+
+export default function App() {
+  return (
+    <PagerView style={styles.pagerView} initialPage={0}>
+      <View key="1">
+        <Text>First page</Text>
+      </View>
+      <View key="2">
+        <Text>Second page</Text>
+      </View>
+    </PagerView>
+  );
+};
+
+const styles = StyleSheet.create({
+  pagerView: {
+    marginTop: 100,
+    backgroundColor: 'red',
+    flex: 1,
+  },
+});
