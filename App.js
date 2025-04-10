ec94423f44729721bbb38d30364c6e7df6f7f1de
Kiran Koravi <kiran.koravi@buzzybrains.com>
2025-04-10 12:44:21 +0530

    Push notification package added

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import React from "react";
-import LottieView from "lottie-react-native";
+import React from 'react';
+import { Alert, View } from 'react-native';
+import {Button} from 'react-native';
+import PushNotification from "react-native-push-notification";
 
-export default function Animation() {
-  return (
-    <LottieView
-      source={require("./loading.json")}
-      style={{width: "100%", height: "100%"}}
-      autoPlay
-      loop={true}
-    />
+
+
+
+export default function App() {
+
+
+		function pushNotif(){
+  PushNotification.createChannel(
+    {
+      channelId: "specialid", // (required)
+      channelName: "Special messasge", // (required)
+      channelDescription: "Notification for special message", // (optional) default: undefined.
+      importance: 4, // (optional) default: 4. Int value of the Android notification importance
+      vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+    },
+    (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
   );
+  PushNotification.localNotification({
+    channelId:'specialid', //his must be same with channelid in createchannel
+    title:'hello',
+    message:'test message'
+  })
+ }
+
+  return <View style={{ marginTop: 100, backgroundColor: "red" }}><Button  onPress={pushNotif} title="click" /></View>
 }
--- ios/MycaApp/AppDelegate.swift
+++ ios/MycaApp/AppDelegate.swift
@@ -4,8 +4,11 @@ import React_RCTAppDelegate
 import ReactAppDependencyProvider
 import Firebase
 
+import UserNotifications
+import RNCPushNotificationIOS
+
 @main
-class AppDelegate: RCTAppDelegate {
+class AppDelegate: RCTAppDelegate, UNUserNotificationCenterDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     self.moduleName = "MycaApp"
     self.dependencyProvider = RCTAppDependencyProvider()
@@ -15,6 +18,8 @@ class AppDelegate: RCTAppDelegate {
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
+    let center = UNUserNotificationCenter.current()
+    center.delegate = self
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
@@ -29,5 +34,36 @@ class AppDelegate: RCTAppDelegate {
 #else
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
+  }
+
+  // Called when successfully registered for remote notifications
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    RNCPushNotificationIOS.didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
+  }
+
+  // Called when failed to register
+  override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+   // RNCPushNotificationIOS.didFailToRegisterForRemoteNotifications(withError: error)
+  }
+
+  // Called when a remote notification is received
+  override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+                   fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    RNCPushNotificationIOS.didReceiveRemoteNotification(userInfo, fetchCompletionHandler: completionHandler)
+  }
+
+  // Called when a notification is tapped by the user
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+    RNCPushNotificationIOS.didReceive(response)
+    completionHandler()
+  }
+
+  // Called when a notification is delivered to a foreground app
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+                              withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    completionHandler([.alert, .sound, .badge])
   }
 }
